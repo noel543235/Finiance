444f7a59cf17a57704f194cadc322776fa679d2e
Christopher Villegas <cvillegasmadera@hotmail.com>
2025-02-13 16:38:42 -0500

    Revert "Add Dynamic Pie chart" to fix an error
    This reverts commit 2596eb876f0c933b04e242ec1cad93b83c2fd89b.

--- savings/views.py
+++ savings/views.py
@@ -1,39 +1,9 @@
-from django.shortcuts import render
-from expenses.models import Expense
-from django.db.models import Sum
-from django.http import JsonResponse
-import json
+from django.http import HttpResponse
+from django.template import loader
+
 
 
 def index(request):
-
-    frequency = "Monthly"
-
-    # Query the database for the top 10 Monthly expenses
-    expenses = Expense.objects.filter(frequency=frequency).order_by('-amount')[:10]
-    
-    # Calculate the sum of all Monthly expenses 
-    total_expenses = Expense.objects.filter(frequency=frequency).aggregate(Sum('amount'))['amount__sum'] or 0
-    
-    # Calculate proportions for each expense relative to the total
-    proportions = [expense.amount/total_expenses * 360 if total_expenses > 0 else 0 for expense in expenses]
-    proportions = [float(proportion) for proportion in proportions]
-    return render(request, "savings/savings.html", {'expenses':expenses, 'proportions': json.dumps(proportions)})
-    
-def get_expenses(request):
-    frequency = request.GET.get('frequency', 'Monthly') 
-
-    # Query the database for the top 10 expenses for that frequency
-    expenses = Expense.objects.filter(frequency=frequency).order_by('-amount')[:10]
-    
-    # Calculate total expenses sum
-    total_expenses = Expense.objects.filter(frequency=frequency).aggregate(Sum('amount'))['amount__sum'] or 0
-
-    # Calculate proportions for chart
-    proportions = [expense.amount / total_expenses * 360 if total_expenses > 0 else 0 for expense in expenses]
-    proportions = [float(proportion) for proportion in proportions]
-    
-    expenses_data = list(expenses.values('label', 'amount'))
-
-    # Convert to JSON format
-    return JsonResponse({'expenses': expenses_data, 'proportions': proportions})
+    template = loader.get_template("savings/savings.html")
+ 
+    return HttpResponse(template.render())

de28ad1d813b93f8fa689c37d22f309a06fdcba5
Riley Sweeting <rileyts2004@gmail.com>
2025-02-20 16:41:30 -0500

    Rough Savings Model Implementation

--- savings/admin.py
+++ savings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Savings
 
 # Register your models here.
+
+class SavingsAdmin(admin.ModelAdmin):
+    model = Savings
+    fieldsets = (
+        (None, {'fields': ('user', 'label', 'amount', 'date', 'frequency')}),
+    )
+
+admin.site.register(Savings, SavingsAdmin)
--- savings/models.py
+++ savings/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Savings(models.Model):
+    
+    FREQUENCY_CHOICES = [
+        ('One-Time', 'One-Time'),
+        ('Weekly', 'Weekly'),
+        ('Monthly', 'Monthly'),
+        ('Yearly', 'Yearly'),
+    ]
+    
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    label = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(default=timezone.now)
+    frequency = models.CharField(max_length=10, choices=FREQUENCY_CHOICES, default='One-Time')
+    start_date = models.DateField(default=timezone.now)
+    
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.label + self.start_date

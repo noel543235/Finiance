20c5db252c88315dd0191e487ff3852f7f38e3bd
Christopher Villegas <cvillegasmadera@hotmail.com>
2025-02-20 16:53:09 -0500

    Add Expense deletion functionality

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ExpenseForm
 from .models import Expense
+
 
 def add_expense(request):
     if request.method == "POST":
@@ -15,6 +16,14 @@ def add_expense(request):
 
     return render(request, "expenses/add_expense.html", {"form": form})
 
+
 def expense_list(request):
     expenses = Expense.objects.filter(user=request.user).order_by('-date')
     return render(request, "expenses/expenses.html", {"expenses": expenses})
+
+
+def delete_expense(request, expense_id):
+    if request.method == "POST":
+        expense = get_object_or_404(Expense, id=expense_id)
+        expense.delete()
+    return redirect('expenses')

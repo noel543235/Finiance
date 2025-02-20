5af60666151ee22a10135d323197ecdb75bea1db
GreenMonkey85 <ccbennett@tampabay.rr.com>
2025-02-19 20:37:17 -0500

    added a future value calculator method to the savings view and I am now testing my "test" branch

--- savings/views.py
+++ savings/views.py
@@ -37,3 +37,30 @@ def get_expenses(request):
 
     # Convert to JSON format
     return JsonResponse({'expenses': expenses_data, 'proportions': proportions})
+
+def future_value_calculator(present_value, compounds, interest_rate, periodic_deposit):
+    '''
+    Parameters: float: present_value, int: compounds, int: interest rate,
+        float: periodic deposit
+
+    Returns: 2D list: compounds_rows
+
+    this method calculates the future value and displays the stats for each
+    compound
+    '''
+    # initialize 2D list: compound_rows and change the interest rate to decimal form
+    compound_rows = []
+    interest_rate = interest_rate/100
+
+    # loop through the number of compounds
+    for i in range(compounds):
+        
+        # calculate the future value for each compound and add the present value,
+        # deposit amount, interest of present value, and the future value
+        future_value = present_value * (1 + interest_rate) + periodic_deposit
+        compound_rows.append([present_value, periodic_deposit, interest_rate*present_value, future_value])
+
+        # set the new present value for the next iteration
+        present_value = future_value
+
+    return compound_rows

90caea907c7290bb323e3ee3f45bcc7c29273ac8
Adriana <aviviano@me.com>
2025-03-11 21:06:55 -0400

    resoucesandhistory

--- finance/settings.py
+++ finance/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'expenses',
     'savings',
     'bootstrap5',
+    'resources',
+    
 ]
 
 MIDDLEWARE = [
--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-
+    path("history", views.history, name="history"),
 ]

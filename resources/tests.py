a15657dde0d99eb9476e63a55f2a08497fdbb1d9
Adriana <aviviano@me.com>
2025-04-08 16:40:12 -0400

    tests for resources

--- resources/tests.py
+++ resources/tests.py
@@ -1,5 +1,21 @@
 from django.test import TestCase
-# from django.urls import reverse
-# from .models  import Item
+from django.urls import reverse
+from  .views import getVids, getImg, getArt
 
 # Create your tests here.
+class ResourcesTestCase(TestCase):
+    def testGetVids(self):
+        print("Videos")
+        vids = getVids()
+        self.assertEqual(len(vids), 10)
+        self.assertTrue(any("youtube"in v.link.lower() for v in vids))
+
+    def testGetArt(self):
+        art = getArt()
+        self.assertEqual(len(art), 8)
+        self.assertTrue(all(a.link.startswith("https://") for a in art))
+
+    def testGetImg(self):
+        img = getImg()
+        self.assertEqual(len(img), 4)
+        self.assertTrue(all(i.link.startswith("https://") for i in img))
